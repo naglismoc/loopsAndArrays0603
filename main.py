fcf15fd1462f17f05dd10cbaf0ec592581427717
naglicmoc <dvd.filmas@gmail.com>
2025-06-16 11:28:54 +0300

    for, while, break, continue

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-for i in range(5):
+import random
+
+from sympy import isprime
+
+for i in range(5): # [0,1,2,3,4]
     print("labas")
     print("rytas")
+    print(i)
 
 print("zemiau ciklo")
 
@@ -29,17 +34,71 @@ print(len(games)) # pasako kiek yra elementu masyve
 for zaidimas in games:
     print("My favorite game is " + zaidimas)
 
+i % 10 == 0
 
 
+count = 0
+for i in range(50):
+    print(i)
+    count += 1
+
+print("prasisuko", count)
+
+count_prime = 0
+for i in range(10,100):
+    if isprime(i):
+        print(i)
+        count_prime += 1
+print("pirminiu skaiciu radome",count_prime)
 
 
+for i in range(10):
+    if i % 2 == 0:
+        continue
+    print(i)
+
+print("---------------------")
+
+for i in range(1,10):
+    if i % 4 == 0:
+        break
+    print(i)
+
+#loop loop
+#while
+
+counter = 0
+while True:
+    counter +=1
+    if counter >= 5:
+        break
+    print(counter)
+print("------------------------")
+while True:
+    rnd_num = random.randint(1,6)
+    if rnd_num == 1:
+        break
+    print(rnd_num)
+
+print("------------------------")
+
+is_even = False
+
+while not is_even:
+    rnd_num = random.randint(0,10)
+    if rnd_num % 2 == 0:
+        is_even = True
+    print(rnd_num)
 
 
+print("------------------------")
 
+is_not_even = True
 
-
-
-
-
+while is_not_even:
+    rnd_num = random.randint(0,10)
+    if rnd_num % 2 == 0:
+        is_not_even = False
+    print(rnd_num)
 
 
